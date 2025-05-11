0a0610097ba067818510bcfd4f62b7b8bb9c866d
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-05-11 23:13:05 +0300

    feat: fetch jobs from external api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const url = 'https://www.course-api.com/react-tabs-project';
+
 function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+
+	const fetchJobs = async () => {
+		const response = await fetch(url);
+		const newJobs = await response.json();
+		setJobs(newJobs);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		fetchJobs();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className="jobs-center">
+				<div className="loading"></div>
+			</section>
+		);
+	}
+
 	return <h1>Jobs Tabs</h1>;
 }
 
